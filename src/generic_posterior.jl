bdadcfc42ac51ba38aebbd30afb66e35368a9a93
Simon Barthelmé <simon.barthelme@gipsa-lab.fr>
2024-04-02 16:44:53 +0200

    Running on latent position estimation

--- src/generic_posterior.jl
+++ src/generic_posterior.jl
@@ -15,6 +15,12 @@ function Base.inv(M :: MyPDMat)
     inv(M.S)
 end
 
+#Add a A*D*A' term to matrix
+function addlowrank!(M :: MyPDMat,A,D)
+    BLAS.gemm!('N','N',1.,A,D*A',1.,M.S)
+end
+
+
 struct LinearMaps{T}
     H :: Vector{T}
 end
@@ -66,6 +72,7 @@ end
 struct GenericApprox{Tsig <: AbstractPDMat,Tm,Th,Ts} <: AbstractGaussApprox
     Σ :: Tsig
     μ :: Vector{Tm}
+    r :: Vector{Tm}
     logz :: Vector{Tm}
     R :: Matrix{Tm}
     H :: Th
@@ -79,17 +86,23 @@ function linearshift(G :: GenericApprox)
     G.Σ\G.μ
 end
 
-function GenericApprox(S :: GenericSites,τ=.01)
+function GenericApprox(S:: GenericSites,τ =0.01)
+    m = npred(S)
+    GenericApprox(S,τ*Matrix(I,m,m))
+end
+
+function GenericApprox(S :: GenericSites,Q0 :: AbstractMatrix)
     n = nsites(S)
     m = npred(S)
     d = dim(S)
-    Q0=τ*Matrix(I,m,m)
+    #Q0=τ*Matrix(I,m,m)
     Σ=MyPDMat(inv(Q0))
     μ=zeros(m)
+    r=zeros(m)
     logz=zeros(n)
     R=zeros(d,n)
     H=LinearMaps([zeros(d,d) for _ in 1:n])
-    GenericApprox(Σ,μ,logz,R,H,S)
+    GenericApprox(Σ,μ,r,logz,R,H,S)
 end
 
 function Statistics.mean(G :: GenericApprox)
@@ -99,23 +112,64 @@ Statistics.cov(G :: GenericApprox) = G.Σ
 
 #naive implementation
 function cavity(G :: GenericApprox,i)
-    Qc = inv(G.Σ) - G.S.A[i]'*G.H[i]*G.S.A[i]
+    Qc = inv(Symmetric(G.Σ)) - G.S.A[i]'*G.H[i]*G.S.A[i]
     mc = Qc\(linearshift(G) - G.S.A[i]'*G.R[:,i])
-    MvNormal(mc,inv(Qc))
+    MvNormal(mc,inv(Symmetric(Qc)))
+    #mv,Qc
+end
+
+function cavity_marginal(G :: GenericApprox,i)
+    Sm = G.S.A[i]*G.Σ*G.S.A[i]'
+    mm = G.S.A[i]*G.μ
+    Qm = inv(Sm)
+    rm = Qm*mm
+    S = inv(Qm - G.H[i])
+    r = rm - G.R[:,i]
+    MvNormal(S*r,S)
+    #mv,Qc
 end
 
 function compute_site_contribution(G::GenericApprox,i)
+    mvc = cavity_marginal(G,i)
+    Qc = inv(mvc.Σ)
+    rc = Qc*mvc.μ
+    mm=TiltedGaussians.moments(mvc,G.S.qr,Base.Fix2(G.S.f,i))
+    Qh = inv(mm.C)
+    δQ = Qh-Qc
+    rh = Qh*mm.m
+    δr = rh - rc
+    δz = log_partition(Qh,rh)-log_partition(Qc,rc)
+    (δz=δz,δr=δr,δQ=δQ)
+end
+
+
+function compute_site_contribution_old(G::GenericApprox,i)
     mc = cavity(G,i)
     TiltedGaussians.contributions_ep(mc,G.S.qr,G.S.A[i],Base.Fix2(G.S.f,i))
 end
 
-function update!(G::GenericApprox,i)
+function update!(G::GenericApprox,i,α=0.0)
     ct = compute_site_contribution(G,i)
-    Qn = inv(G.Σ) + G.S.A[i]'*(ct.δQ - G.H[i])*G.S.A[i]
-    G.H[i] = ct.δQ
-    rn = linearshift(G) + G.S.A[i]'*(ct.δr - G.R[:,i])
-    G.R[:,i] = ct.δr
-    S = inv(Qn)
-    G.Σ .= S
-    G.μ .= S*sum((G.S.A[i]'*(G.R[:,i]) for i in 1:nsites(G)))
+    D=(1-α)*(ct.δQ - G.H[i])
+    Z = Matrix(G.Σ*G.S.A[i]')
+
+    G.H[i] = α*G.H[i] + (1-α)*ct.δQ
+    #rn = linearshift(G) + G.S.A[i]'*(1-α)*(ct.δr - G.R[:,i])
+    dr = G.S.A[i]'*(1-α)*(ct.δr - G.R[:,i])
+    G.R[:,i] = α*G.R[:,i]+ (1-α)*ct.δr
+    addlowrank!(G.Σ,Z,-inv(inv(D)+G.S.A[i]*Z))
+    G.r .+= dr
+#    @assert G.r ≈ sum((G.S.A[i]'*(G.R[:,i]) for i in 1:nsites(G)))
+    mp  =G.Σ*sum((G.S.A[i]'*(G.R[:,i]) for i in 1:nsites(G)))
+    G.μ .= G.Σ*G.r
+#    @show norm(G.μ - mp)
 end
+
+function run_ep!(G;α=0.0,npasses=4,schedule=1:nsites(G))
+    for ip in 1:npasses
+        for i in schedule
+            update!(G,i,α)
+        end
+    end
+end
+
